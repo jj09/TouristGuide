03f9773f896f72ab7c0d67aa2673c4842b750592
jj09_cp <SND\jj09_cp>
2012-08-04 09:48:16 +0000

    update tg service

--- TouristGuide/TouristGuide.Service/WPService.svc.cs
+++ TouristGuide/TouristGuide.Service/WPService.svc.cs
@@ -1,37 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Services;
-using System.Data.Services.Common;
 using System.Linq;
-using System.ServiceModel.Web;
-using System.Web;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
 using TouristGuide.Core.Repository;
 using TouristGuide.Core.Models;
 
 namespace TouristGuide.Service
 {
-    public class WPService : IWPService
-    {
-        private readonly IAttractionRepository attractionRepository;
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WPService" in code, svc and config file together.
+	public class WPService : IWPService
+	{
+		private readonly IAttractionRepository attractionRepository;
 
-        public WPService()
-        {
-            this.attractionRepository = new AttractionRepository(new TouristGuideDB());
-        }
+		public WPService()
+		{
+			this.attractionRepository = new AttractionRepository(new TouristGuideDB());
+		}
 
-        public WPService(IAttractionRepository attractionRepository)
-        {
-            this.attractionRepository = attractionRepository;
-        }
+		public WPService(IAttractionRepository attractionRepository)
+		{
+			this.attractionRepository = attractionRepository;
+		}
 
-        public List<Attraction> GetAttractions(string place, int start, int count)
-        {
-            return attractionRepository.GetAttractions(place, start, count).OrderByDescending(x => x.AvgRating).ToList();
-        }
-
-        public string Name()
-        {
-            return "WP7 Service";
-        }
-    }
+		public List<Attraction> GetAttractions(string place, int start, int count)
+		{
+			return attractionRepository.GetAttractions(place, start, count).OrderByDescending(x => x.AvgRating).ToList();
+		}
+	}
 }
